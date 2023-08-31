1ffaae071ce0beda833319f474e24c192e1134bf
Taha Qutbuddin <tahashakir40@gmail.com>
2023-08-31 05:25:21 -0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -14,15 +14,15 @@ define("DATABASE", "hannah2");    // The database name
 
 
 // Google API configuration
-define("GOOGLE_CLIENT_ID" , "1081573984725-etu33sc86b78biarf409njk4vvjtaivo.apps.googleusercontent.com");
-define("GOOGLE_CLIENT_SECRET","XzRa0Z9ho12XSHl5zaFwC231");
-define("GOOGLE_REDIRECT_URL","https://chef.flavourflip.de/googlelogin/redirect.php");
+define("GOOGLE_CLIENT_ID" , "");
+define("GOOGLE_CLIENT_SECRET","");
+define("GOOGLE_REDIRECT_URL","");
 
 // Facebook API configuration
-define('FB_APP_ID', '1384360135257914');
+define('FB_APP_ID', '');
 define('FB_APP_VERSION','v10.0');
-define('FB_APP_SECRET', '534370044b6397da07695635b2e69fdc');
-define('FB_REDIRECT_URL', 'https://chef.flavourflip.de/facebooklogin/redirect.php');
+define('FB_APP_SECRET', '');
+define('FB_REDIRECT_URL', '');
 
 // Start session
 if(!session_id()){
